881d49fc024908654122bb6611081bada59f2113
Artem Karmaz <inste@icloud.com>
2019-03-21 15:17:45 +0200

    Initial Commit

---
+++ UITableViewWithUIpageViewWithCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewWithUIpageViewWithCode
+//
+//  Created by Artem Karmaz on 3/21/19.
+//  Copyright © 2019 Johansson Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
